2821247f9fb4fcbcf4f22b82d742e5da61b8fe83
lgf-136 <86616706+lgf-136@users.noreply.github.com>
2022-09-19 01:02:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learn-redux-toolkit-vite
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/learn-redux-toolkit-vite)
---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './App';
+import './index.css';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
